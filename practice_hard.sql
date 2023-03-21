fe39643aaf89945a17954904d1aee306100d5a85
Liliana Ilut <liliana.ilut@gmail.com>
2023-03-21 13:35:58 -0500

    question3

--- practice_hard.sql
+++ practice_hard.sql
@@ -15,7 +15,7 @@ order by weight_group desc
 
 --alternate solutions:
 SELECT
-  TRUNCATE(weight, -1) AS weight_group,
+  --TRUNCATE(weight, -1) AS weight_group,
   count(*)
 FROM patients
 GROUP BY weight_group
@@ -53,3 +53,21 @@ SELECT
   height,
   weight / power(CAST(height AS float) / 100, 2) >= 30 AS obese
 FROM patients
+
+--Question 3
+-- Show patient_id, first_name, last_name, and attending doctor's specialty.
+-- Show only the patients who has a diagnosis as 'Epilepsy' and the doctor's first name is 'Lisa'
+-- Check patients, admissions, and doctors tables for required information.
+
+SELECT 
+	p.patient_id,
+    p.first_name,
+    p.last_name,
+    d.specialty
+from patients As p 
+Join admissions as a on p.patient_id = a.patient_id
+JOIN doctors as d on a.attending_doctor_id = d.doctor_id
+where
+	a.diagnosis = 'Epilepsy'
+    and d.first_name = 'Lisa'
+;
