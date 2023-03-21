a34bec76748b3bbe35f39ab9ba1b64720495a4d7
Liliana Ilut <liliana.ilut@gmail.com>
2023-03-21 16:08:01 -0500

    added hard section

--- northwind_DB.sql
+++ northwind_DB.sql
@@ -107,5 +107,66 @@ from products;
 --QUESTION 1
 --Show the ProductName, CompanyName, CategoryName from the products, suppliers, and categories table
 
+SELECT 
+	p.product_name,
+    s.company_name,
+    c.category_name
+from categories as c 
+join products as p 
+	on c.category_id = p.category_id
+join suppliers as s 
+	on p.supplier_id = s.supplier_id
+;
 
+--QUESTION 2
+--Show the category_name and the average product unit price for each category rounded to 2 decimal places.
 
+SELECT 
+	c.category_name,
+    round(avg(p.unit_price),2) as avg_unit_price
+from categories as c 
+join products as p 
+	on c.category_id = p.category_id
+group by c.category_name
+;
+
+--QUESTION 3
+--Show the city, company_name, contact_name from the customers and suppliers table merged together.
+-- Create a column which contains 'customers' or 'suppliers' depending on the table it came from.
+
+SELECT 
+	c.city,
+    c.company_name,
+    c.contact_name,
+    'customers' as relationship
+from customers as c 
+union all
+select
+	s.city,
+    s.company_name,
+    s.contact_name,
+    'suppliers' as relationship
+from suppliers as s 
+;
+     
+------------------------------------------------------------------------------------------------
+--HARD SECTION----HARD SECTION----HARD SECTION----HARD SECTION----HARD SECTION--
+
+-- Show the employee's first_name and last_name, a "num_orders" column with a count of the orders taken, and a column called "Shipped" that displays "On Time" if the order shipped on time and "Late" if the order shipped late.
+-- Order by employee last_name, then by first_name, and then descending by number of orders.
+
+SELECT 
+	e.first_name,
+    e.last_name,
+    count(o.order_id) as num_orders,
+    (Case 
+     	when o.shipped_date < o.required_date then 'On Time'
+     else 'Late'
+     end) as shipped 
+from employees as e 
+join orders as o 
+	on e.employee_id = o.employee_id
+group by e.first_name, e.last_name, shipped    
+order by e.last_name, e.first_name, num_orders desc
+;
+     
