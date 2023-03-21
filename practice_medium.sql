1747130ebc99099d1d490fd33af07c1eed622b22
Liliana Ilut <liliana.ilut@gmail.com>
2023-03-21 11:48:13 -0500

    added Q4 and Q5

--- practice_medium.sql
+++ practice_medium.sql
@@ -35,3 +35,37 @@ from patients As p
 join admissions As a ON p.patient_id = a.patient_id
 join doctors as d on a.attending_doctor_id = d.doctor_id
 ;
+
+--Question 4
+--Display the number of duplicate patients based on their first_name and last_name.
+
+SELECT 
+	first_name,
+    last_name,
+    count(*) as num_duplicate_pat
+from patients
+group by first_name, last_name
+having count(*) > 1
+;
+
+--Question 5
+-- Display patient's full name,
+-- height in the units feet rounded to 1 decimal,
+-- weight in the unit pounds rounded to 0 decimals,
+-- birth_date,
+-- gender non abbreviated.
+
+-- Convert CM to feet by dividing by 30.48.
+-- Convert KG to pounds by multiplying by 2.205.
+
+SELECT 
+	concat(first_name, ' ', last_name) as full_name,
+    round((height / 30.48), 1) as height_feet,
+    round(weight * 2.205) as weight_pounds,
+    birth_date,
+    CASE
+ 		when gender = 'M' then 'MALE'
+    	WHEN gender = 'F' then 'FEMALE'
+ 	END as gender
+from patients
+;
