c06975654d075e7f17d5c11dd68c32aa4fb06194
Liliana Ilut <liliana.ilut@gmail.com>
2023-03-22 10:03:44 -0500

    added Q5

--- hacker_rank.sql
+++ hacker_rank.sql
@@ -28,3 +28,17 @@ WHERE
 
 SELECT DISTINCT City FROm Station WHERE LEFT(City,1) NOT IN ('a','e','i','o','u')
 
+--QUESTION 5
+-- Query the list of CITY names from STATION that either do not start with vowels or do not end with vowels. Your result cannot contain duplicates.
+
+SELECT 
+    DISTINCT city 
+FROm Station 
+WHERE 
+    RIGHT(City,1) NOT IN ('a','e','i','o','u')
+    or LEFT(City,1) NOT IN ('a','e','i','o','u');
+
+--QUESTION 6
+--Query the Name of any student in STUDENTS who scored higher than  Marks. Order your output by the last three characters of each name. If two or more students both have names ending in the same last three characters (i.e.: Bobby, Robby, etc.), secondary sort them by ascending ID.
+
+
